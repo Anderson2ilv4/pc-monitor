b4b9a9ccd38086e7aff6ffb85f93d39fdfaf2c67
Anderson2il4 <silvaanderson124@gmail.com>
2024-01-27 22:28:50 -0300

    usage-update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import platform
 import cpuinfo
 import psutil
+from time import sleep
 
 def system_info():
     print('OS INFO'.center(32))
@@ -16,11 +17,17 @@ def hardware_info():
     print(f'Cores: {psutil.cpu_count()}')
     print(f'Memory: {psutil.virtual_memory().total // 1024 // 1024 // 1024}')
 
+def usage_monitor(cpu_usage, memory_usage, bars=50):
+    cpu_percent = (cpu_usage / 100.0)
+    cpu_bar = '▮' * int(cpu_percent * bars) + '-' * (bars - int(cpu_percent * bars))
+    memory_percent = (memory_usage / 100.0)
+    memory_bar = '▮' * int(memory_percent * bars) + '-' * (bars - int(memory_percent * bars))
+    print(f'\rCPU usage: |{cpu_bar}| {cpu_usage:.2f}%  ', end=' ')
+    print(f'Memory usage: |{memory_bar}| {memory_usage:.2f}%  ', end='\r')
+
 system_info()
 hardware_info()
+while True:
+    usage_monitor(psutil.cpu_percent(), psutil.virtual_memory().percent, 30)
+    sleep(0.5)
     
-
-
-
-
-
